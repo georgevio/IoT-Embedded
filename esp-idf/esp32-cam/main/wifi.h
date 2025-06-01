916afb5b21b75ca349567225ffd90f39d642f6c4
George Violettas <georgevio@gmail.com>
2025-06-01 17:52:08 +0200

    sdkconfig and managed_components removed from gitignore

---
+++ esp-idf/esp32-cam/main/wifi.h
@@ -0,0 +1,14 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+#include "esp_err.h"
+
+/**
+ * @brief Initialize WiFi in Station mode.
+ * 
+ * // @return esp_err_t turned into void
+ */
+//esp_err_t 
+void wifi_init_sta(void);
+
+#endif // WIFI_H
